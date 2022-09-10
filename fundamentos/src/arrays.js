443f93dd5d0619b1c0486b166a04e6d8ec46f90a
Adriel Carmo <adrieldovale7@gmail.com>
2022-09-10 00:56:30 -0400

    Manipulando dados com Arrays em ECMAScript.

---
+++ fundamentos/src/arrays.js
@@ -0,0 +1,19 @@
+// Estrutura de Array de Objetos
+const alunos = [
+    {
+        nome: 'Adriel',
+        idade: 19
+    },
+    {
+        nome: 'Cleiton',
+        idade: 27
+    },
+];
+
+// Adicionando novo obj. no Array
+alunos.push({
+    nome: "Maria",
+    idade: 15
+});
+
+console.log(alunos[1].idade);
