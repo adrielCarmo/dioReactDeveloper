7a207ecc5d5daae6d2eea6228a2f3726e6923ac1
Adriel Carmo <adrieldovale7@gmail.com>
2022-09-10 01:39:36 -0400

    Reorganização de diretórios do Projeto.

--- fundamentos/src/variaveis.js
+++
@@ -1,8 +0,0 @@
-const nome = 'Adriel'; //  não pode ser alterada diretamente
-let aprovado = true; // mais utilizada
-var name = "Cleitinho"; // não é mais recomendada, pois é acessível fora do escopo
-let nota = 8;
-
-console.log(typeof(nota)); // retorna o tipo da variavel
-
-console.log(`O tipo da variável "name" é ${typeof(nota)}!`)
