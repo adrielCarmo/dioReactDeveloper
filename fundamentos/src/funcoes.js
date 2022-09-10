35fb67b88da4027f883fabed94073ab48237e5b7
Adriel Carmo <adrieldovale7@gmail.com>
2022-09-10 00:47:07 -0400

    Exemplos utilizando Arrow Functions em ECMAScript.

---
+++ fundamentos/src/funcoes.js
@@ -0,0 +1,13 @@
+// Estrutura de uma FUNÇÃO EM JS
+function somar(num1, num2) {
+    return num1 + num2;
+}
+
+const resultado1 = somar(10,5);
+console.log(`O resultado da soma é: ${resultado1}`);
+
+
+// Estrutura em ECMAScript (Arrow Functions)
+const soma = (num1, num2) => num1 + num2;
+
+console.log(soma(10,10));
